cce7c73d289a60dd6a45767d6968366ab871b66c
Sojiwoo <58384366+jiwooproity@users.noreply.github.com>
2022-04-16 20:27:41 +0900

    DEVELOP-004
    Get Movie API

--- src/components/page/main/Main.js
+++ src/components/page/main/Main.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 const MainContainer = styled.div`
   width: 100%;
@@ -19,10 +20,30 @@ const MainTestSpan = styled.span`
   color: white;
 `;
 
+const MovieImage = styled.img`
+  width: 300px;
+
+  object-fit: cover;
+`;
+
 const Main = () => {
+  const [data, setData] = useState([]);
+
+  console.log(data);
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const movie = await axios.get("https://yts.mx/api/v2/list_movies.json?limit=50&sort_by=download_count");
+
+      setData(movie.data.data.movies);
+    };
+
+    getMovie();
+  }, []);
+
   return (
     <MainContainer>
-      <MainTestSpan>good</MainTestSpan>
+      <MainTestSpan>{data && data.length > 0 && data.map((item) => <MovieImage src={item.large_cover_image} />)}</MainTestSpan>
     </MainContainer>
   );
 };
